4214b130ddd5477347d5d8311850de016cbe5aee
Pedro Igor <pedroigor.silva@outlook.com>
2022-11-10 11:10:04 -0300

    feat: fix titles and salaries counting on mongo

--- src/pages/api/[db]/salaries/count.js
+++ src/pages/api/[db]/salaries/count.js
@@ -2,12 +2,20 @@
 import middleware, {switchHandlers} from '../../../../utils/middleware';
 import {Salaries as PostgresSalaries}
   from '../../../../models/postgres/salary';
+import {Employees} from '../../../../models/mongo/employee';
 
 export default middleware(async (req, res) => {
   await switchHandlers(req, res, mongo, postgres);
 });
 const mongo = async (req, res) => {
-
+  const {count} = (await Employees.aggregate([
+    {
+      $group: {
+        _id: null, count: {$sum: {$size: '$salaries'}},
+      },
+    },
+  ]))[0];
+  res.response = count;
 };
 const postgres = async (req, res) => {
   const count = await PostgresSalaries.count();
--- src/pages/api/[db]/titles/count.js
+++ src/pages/api/[db]/titles/count.js
@@ -2,12 +2,20 @@
 import middleware, {switchHandlers} from '../../../../utils/middleware';
 import {Titles as PostgresTitles}
   from '../../../../models/postgres/title';
+import {Employees} from '../../../../models/mongo/employee';
 
 export default middleware(async (req, res) => {
   await switchHandlers(req, res, mongo, postgres);
 });
 const mongo = async (req, res) => {
-
+  const {count} = (await Employees.aggregate([
+    {
+      $group: {
+        _id: null, count: {$sum: {$size: '$titles'}},
+      },
+    },
+  ]))[0];
+  res.response = count;
 };
 const postgres = async (req, res) => {
   const count = await PostgresTitles.count();
