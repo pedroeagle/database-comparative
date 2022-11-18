c3297bbb39ec6e5e4b8818fd5aea495f89fd8668
Pedro Igor <pedroigor.silva@outlook.com>
2022-11-18 16:47:41 -0300

    feat: store csv file with user id

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,4 +1,7 @@
 import {use} from 'next-api-middleware';
+const fs = require('fs-extra');
+const converter = require('json-2-csv');
+const parse = require('csvtojson/v2');
 
 const getTiming = async (req, res, next) => {
   const {query: {db}} = req;
@@ -12,9 +15,15 @@ const getTiming = async (req, res, next) => {
 };
 
 const storeResponseTime = async (req, {start, end, time}) => {
-  const {query: {id, db}, url} = req;
-  console.log({id});
+  const {query: {id, db, ...query}, url} = req;
+  await fs.ensureFile(`reports/${id}.csv`);
+
+  const csv = await parse().fromFile(`reports/${id}.csv`);
   const path = url.replace(`/api/${db}`, '').match(/[^?]+/)[0];
+
+  converter.json2csv([...csv, {start, end, time, db, path, ...query}], (_, res) => {
+    fs.writeFileSync(`reports/${id}.csv`, res);
+  });
 };
 
 export const switchHandlers = async (req, res, mongo, postgres) => {
